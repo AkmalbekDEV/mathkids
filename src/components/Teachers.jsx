887875a340a5c2112d98350a8035a0320a9303a0
AkmalbekDEV <akmalyusufov32@gmail.com>
2024-09-21 22:35:47 +0500

    teachers page changed

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -32,12 +32,12 @@ const Teachers = () => {
       <div className='py-20 grid gap-5'>
         <h1 className='text-5xl max-sm:text-center max-sm:px-8 max-sm:text-4xl font-medium'>{t("teachers.title")}</h1>
         <h1 className='text-xl max-sm:text-center max-sm:px-8'>{t("teachers.desc")}</h1>
-        <div className='flex items-center flex-wrap gap-10'>
+        <div className='flex items-center justify-center flex-wrap gap-10'>
           {data.map((item) => (
-            <div key={item.id} className='grid gap-3 w-[280px] border-2 rounded-xl p-5 max-lg:mx-5'>
+            <div key={item.id} className='grid content-between w-[280px] h-[450px] border-2 rounded-xl p-5 max-lg:mx-5'>
               <img src={item.imageURL} alt={item.name} className='w-[250px] h-[250px] rounded-xl' />
-              <h1 className='text-center font-medium text-2xl'>{item.name}</h1>
-              <h1 className='text-center text-xl'>{item.job}</h1>
+              <h1 className='text-center font-medium text-xl'>{item.name}</h1>
+              <h1 className='text-center text-lg'>{item.job}</h1>
               <Button onClick={() => handleOpenModal(item)} colorScheme='purple'>{t("teachers.btn")}</Button>
             </div>
           ))}
